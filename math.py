5f3787677e09bd86886487a49e1321f0d5a59e13
TimZmD <80001243+TimZmD@users.noreply.github.com>
2025-07-01 09:03:51 +0200

    Fix local math module conflict

--- math.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-
-def dist(self, p1, p2):
-    return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
---
+++ math_utils.py
@@ -0,0 +1,6 @@
+import math
+
+
+def dist(p1, p2):
+    """Calculate Euclidean distance between two 2D points."""
+    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
